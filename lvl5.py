7f3e809b8d3475e3b0e29f69196528ba567a6565
Hamza <hamzamurghay@gmail.com>
2023-11-30 13:22:29 +0530

    puzzle functionality as a click only, image changing puzzle is completed, all technical aspects are finished, only importing finalised drawings left

--- lvl5.py
+++ lvl5.py
@@ -9,118 +9,45 @@ clock = pg.time.Clock()
 
 pg.init()
 
-font = pg.font.SysFont('Consolas', 90)
-
 # Creating assets -------------------------------------------------------------------------------------
+
+light_on = pg.transform.scale(pg.image.load('graphics/8-Bit Pixel Game/lvl5/light_on.png'), (60, 80))
+light_off = pg.transform.scale(pg.image.load('graphics/8-Bit Pixel Game/lvl5/light_off.png'), (60, 80))
+
+light_state = [light_off, light_on]
 
 click_cursor = pg.image.load('graphics/8-Bit Pixel Game/Pointing_hand_cursor.svg.png').convert_alpha()
 change_mouse = False
 
-
 # Function Definitions --------------------------------------------------------------------------------
 
 
-def update_lights(light_list):
-    light_line1 = font.render(f"{' '.join(str(l) for l in light_list[0:4])}", True, 'black')
-    light_line2 = font.render(f"{' '.join(str(l) for l in light_list[4:8])}", True, 'black')
-    light_line3 = font.render(f"{' '.join(str(l) for l in light_list[8:12])}", True, 'black')
-    light_line4 = font.render(f"{' '.join(str(l) for l in light_list[12:16])}", True, 'black')
+def display_light_grid(light_list):
+    light_grid = [light_state[light] for light in light_list]
+    light_grid_positions = [(55+current_column_y*140, 30+current_row_x*140) for current_row_x in range(4) for current_column_y in range(4)]
 
-    screen.blit(light_line1, (115, 95))
-    screen.blit(light_line2, (115, 195))
-    screen.blit(light_line3, (115, 295))
-    screen.blit(light_line4, (115, 395))
+    screen.blits(
+        [(surface, position) for surface, position in zip(light_grid, light_grid_positions)]
+    )
 
 
-def check_for_light_press(mapping_list, light_list):
-    global change_mouse
+def change_light_list(light_clicked):
+    for light in list_of_mappings[light_clicked]:
+        lights[light] = int(not lights[light])
 
-    if pg.Rect(119, 104, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(217, 104, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(315, 104, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(413, 104, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(119, 204, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(217, 204, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(315, 204, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(413, 204, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(119, 304, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(217, 304, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(315, 304, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(413, 304, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(119, 404, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(217, 404, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(315, 404, 42, 58).collidepoint(pg.mouse.get_pos()) or \
-            pg.Rect(413, 404, 42, 58).collidepoint(pg.mouse.get_pos()):
 
-        pg.mouse.set_visible(False)
-        change_mouse = True
-    else:
-        pg.mouse.set_visible(True)
-        change_mouse = False
+def check_for_cursor_hover_and_click(click_event):
+    for row_no in range(4):
+        for column_no in range(4):
 
-    if event.type == pg.MOUSEBUTTONUP and pg.Rect(119, 104, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[0]:
-            light_list[light] = int(not light_list[light])
+            if pg.Rect(55+(row_no*140), 30+(column_no*140), 60, 80).collidepoint(pg.mouse.get_pos()):
+                pg.mouse.set_visible(False)
+                if click_event.type == pg.MOUSEBUTTONUP:
+                    change_light_list(row_no + column_no * 4)  # Getting the current list element address using only its row and column number by use of the formula current_row_number * size of grid + current_column_number
+                return True
 
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(217, 104, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[1]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(315, 104, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[2]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(413, 104, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[3]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(119, 204, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[4]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(217, 204, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[5]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(315, 204, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[6]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(413, 204, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[7]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(119, 304, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[8]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(217, 304, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[9]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(315, 304, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[10]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(413, 304, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[11]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(119, 404, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[12]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(217, 404, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[13]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(315, 404, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[14]:
-            light_list[light] = int(not light_list[light])
-
-    elif event.type == pg.MOUSEBUTTONUP and pg.Rect(413, 404, 42, 58).collidepoint(pg.mouse.get_pos()):
-        for light in mapping_list[15]:
-            light_list[light] = int(not light_list[light])
+    pg.mouse.set_visible(True)
+    return False
 
 
 # Setting up Program Variables ------------------------------------------------------------------------
@@ -128,8 +55,8 @@ def check_for_light_press(mapping_list, light_list):
 lights = [0] * 16
 starting_configurations = [(4, 8, 11), (1, 6, 9), (0, 5, 8, 11), (3, 5, 13), (0, 6)]
 
-for config in starting_configurations[random.randint(0, 4)]: lights[config] = 1
-
+for config in starting_configurations[random.randint(0, 4)]:
+    lights[config] = 1
 
 first_seq = [1, 4, 7, 10, 13, 16]
 second_seq = [2, 5, 8, 11, 14]
@@ -166,10 +93,10 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
             exit()
-        check_for_light_press(list_of_mappings, lights)
+        change_mouse = check_for_cursor_hover_and_click(event)
 
-    screen.fill('darkgrey')
-    update_lights(lights)
+    screen.fill('purple')
+    display_light_grid(lights)
 
     if change_mouse:
         screen.blit(click_cursor, (pg.mouse.get_pos()[0] - 15, pg.mouse.get_pos()[1]))
@@ -182,7 +109,7 @@ while True:
 # 1) Printing Light Mapping Matrix: ----------------------------------------------------------------------
 
 
-# for light_list in temp_all:
+# for light_list in list_of_mappings:
 #     matrix_row = [0]*16
 #     for light_no in light_list:
 #         matrix_row[light_no] = 1
@@ -196,47 +123,47 @@ while True:
 
 # Algorithm for sum of 2 keys:
 
-# for light_mapping_list in temp_all:
-#     for i in range(temp_all.index(light_mapping_list)+1, 16):
+# for light_mapping_list in list_of_mappings:
+#     for i in range(list_of_mappings.index(light_mapping_list)+1, 16):
 #         for pos in light_mapping_list:
 #             lights[pos] = int(not lights[pos])
-#         for second_operand_pos in temp_all[i]:
+#         for second_operand_pos in list_of_mappings[i]:
 #             lights[second_operand_pos] = int(not lights[second_operand_pos])
-#         if lights.count(1) <= 3: print(f"Lights after {temp_all.index(light_mapping_list) + 1} + {i+1} = {lights}")
+#         if lights.count(1) <= 3: print(f"Lights after {list_of_mappings.index(light_mapping_list) + 1} + {i+1} = {lights}")
 #         lights = [0]*16
 
 
 # Algorithm for sum of 3 keys:
 
-# for light_mapping_list in temp_all:
-#     for i in range(temp_all.index(light_mapping_list)+1, 16):
-#         for j in range(temp_all.index(light_mapping_list) + 1, 16):
+# for light_mapping_list in list_of_mappings:
+#     for i in range(list_of_mappings.index(light_mapping_list)+1, 16):
+#         for j in range(list_of_mappings.index(light_mapping_list) + 1, 16):
 #             for pos in light_mapping_list:
 #                 lights[pos] = int(not lights[pos])
-#             for second_operand_pos in temp_all[i]:
+#             for second_operand_pos in list_of_mappings[i]:
 #                 lights[second_operand_pos] = int(not lights[second_operand_pos])
-#             for third_operand_pos in temp_all[j]:
+#             for third_operand_pos in list_of_mappings[j]:
 #                 lights[third_operand_pos] = int(not lights[third_operand_pos])
 #             if lights.count(1) <= 3:
-#                 print(f"Lights after {temp_all.index(light_mapping_list) + 1} + {i+1} + {j+1} = {lights}")
+#                 print(f"Lights after {list_of_mappings.index(light_mapping_list) + 1} + {i+1} + {j+1} = {lights}")
 #             lights = [0]*16
 
 # Algorithm for sum of 4 keys:
 
-# for light_mapping_list in temp_all:
-#     for i in range(temp_all.index(light_mapping_list)+1, 16):
-#         for j in range(temp_all.index(light_mapping_list) + 1, 16):
-#             for k in range(temp_all.index(light_mapping_list) + 1, 16):
+# for light_mapping_list in list_of_mappings:
+#     for i in range(list_of_mappings.index(light_mapping_list)+1, 16):
+#         for j in range(list_of_mappings.index(light_mapping_list) + 1, 16):
+#             for k in range(list_of_mappings.index(light_mapping_list) + 1, 16):
 #                 for pos in light_mapping_list:
 #                     lights[pos] = int(not lights[pos])
-#                 for second_operand_pos in temp_all[i]:
+#                 for second_operand_pos in list_of_mappings[i]:
 #                     lights[second_operand_pos] = int(not lights[second_operand_pos])
-#                 for third_operand_pos in temp_all[j]:
+#                 for third_operand_pos in list_of_mappings[j]:
 #                     lights[third_operand_pos] = int(not lights[third_operand_pos])
-#                 for fourth_operand_pos in temp_all[k]:
+#                 for fourth_operand_pos in list_of_mappings[k]:
 #                     lights[fourth_operand_pos] = int(not lights[fourth_operand_pos])
 #                 if lights.count(1) <= 3:
-#                     print(f"Lights after {temp_all.index(light_mapping_list) + 1} + {i+1} + {j+1} + {k+1} = {lights}")
+#                     print(f"Lights after {list_of_mappings.index(light_mapping_list) + 1} + {i+1} + {j+1} + {k+1} = {lights}")
 #                 lights = [0]*16
 
 # As you can see there is a pattern to this algorithm, so it is easy for you to extend it to higher sums, just be sure
@@ -244,26 +171,26 @@ while True:
 
 # ALGORITHM FOR 6 KEYS:   [I absolutely LOVE this algo, it's so OP, like forget getting 2 key flipped cases(not that those aren't good, they're AMAZING but), you can literally solve the whole puzzle by putting in your state]
 
-# for light_mapping_list in temp_all:
-#     for i in range(temp_all.index(light_mapping_list)+1, 16):
-#         for j in range(temp_all.index(light_mapping_list) + 1, 16):
-#             for k in range(temp_all.index(light_mapping_list) + 1, 16):
-#                 for l in range(temp_all.index(light_mapping_list) + 1, 16):
-#                     for m in range(temp_all.index(light_mapping_list) + 1, 16):
+# for light_mapping_list in list_of_mappings:
+#     for i in range(list_of_mappings.index(light_mapping_list)+1, 16):
+#         for j in range(list_of_mappings.index(light_mapping_list) + 1, 16):
+#             for k in range(list_of_mappings.index(light_mapping_list) + 1, 16):
+#                 for l in range(list_of_mappings.index(light_mapping_list) + 1, 16):
+#                     for m in range(list_of_mappings.index(light_mapping_list) + 1, 16):
 #                             for pos in light_mapping_list:
 #                                 lights[pos] = int(not lights[pos])
-#                             for second_operand_pos in temp_all[i]:
+#                             for second_operand_pos in list_of_mappings[i]:
 #                                 lights[second_operand_pos] = int(not lights[second_operand_pos])
-#                             for third_operand_pos in temp_all[j]:
+#                             for third_operand_pos in list_of_mappings[j]:
 #                                 lights[third_operand_pos] = int(not lights[third_operand_pos])
-#                             for fourth_operand_pos in temp_all[k]:
+#                             for fourth_operand_pos in list_of_mappings[k]:
 #                                 lights[fourth_operand_pos] = int(not lights[fourth_operand_pos])
-#                             for fifth_operand_pos in temp_all[l]:
+#                             for fifth_operand_pos in list_of_mappings[l]:
 #                                 lights[fifth_operand_pos] = int(not lights[fifth_operand_pos])
-#                             for sixth_operand_pos in temp_all[m]:
+#                             for sixth_operand_pos in list_of_mappings[m]:
 #                                 lights[sixth_operand_pos] = int(not lights[sixth_operand_pos])
 #                             if lights == [0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1]:
-#                                 print(f"Lights after {temp_all.index(light_mapping_list) + 1} + {i+1} + {j+1} + {k+1} + {l+1} + {m+1} = {lights}")
+#                                 print(f"Lights after {list_of_mappings.index(light_mapping_list) + 1} + {i+1} + {j+1} + {k+1} + {l+1} + {m+1} = {lights}")
 #                             lights = [0]*16
 
 
