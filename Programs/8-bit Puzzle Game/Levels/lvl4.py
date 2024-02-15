d581bc2429c1806f45c9aa70f20093f68fbec1d7
HamzaMurghay <hamzamurghay@gmail.com>
2024-02-15 08:27:35 +0530

    more lvl4 updates

--- Programs/8-bit Puzzle Game/Levels/lvl4.py
+++ Programs/8-bit Puzzle Game/Levels/lvl4.py
@@ -2,7 +2,7 @@ import pygame as pg
 
 # Setting Up Pygame Pre-requisites -----------------------------------------------------------------------------
 
-screen = pg.display.set_mode((600, 600))
+screen = pg.display.set_mode((1920, 1080))
 pg.display.set_icon(pg.image.load('../../../assets/graphics/8-Bit Pixel Game/lvl4/past_maze.png'))
 clock = pg.time.Clock()
 
@@ -11,33 +11,40 @@ pg.init()
 # Setting up Game Elements -------------------------------------------------------------------------------------
 
 maze = pg.transform.scale(pg.image.load(
-    '../../../assets/graphics/8-Bit Pixel Game/lvl4/maze.png').convert_alpha(), (2200, 2200))
+    '../../../assets/graphics/8-Bit Pixel Game/lvl4/maze.png').convert_alpha(), (3200, 3200))
 
 maze_mask = pg.mask.from_surface(maze, 0)
 maze_mask_surface = maze_mask.to_surface()
 
-
 maze_3d_effect = pg.transform.scale(pg.image.load(
-    '../../../assets/graphics/8-Bit Pixel Game/lvl4/maze_3D_effect.png').convert_alpha(), (2200, 2200))
-
+    '../../../assets/graphics/8-Bit Pixel Game/lvl4/maze_3D_effect.png').convert_alpha(), (3200, 3200))
 
 character_surf = pg.transform.scale(
-    pg.image.load("../../../assets/graphics/8-Bit Pixel Game/lvl4/character.png").convert_alpha(), (10, 10))
-
+    pg.image.load("../../../assets/graphics/8-Bit Pixel Game/lvl4/character.png").convert_alpha(), (15, 20))
 character_mask = pg.mask.from_surface(character_surf)
+
+character_sprite_sheet = pg.image.load(
+    "../../../assets/graphics/8-Bit Pixel Game/lvl4/temp_character_sprite_sheet.png").convert_alpha()
 
 yellow_key = pg.transform.scale(
     pg.image.load("../../../assets/graphics/8-Bit Pixel Game/lvl4/yellow_key.png").convert_alpha(), (35, 35))
-yellow_key_mask = pg.mask.from_surface(yellow_key)
 
-sprite_sheet_yellow = pg.image.load(
+yellow_key_sprite_sheet = pg.image.load(
     "../../../assets/graphics/8-Bit Pixel Game/lvl4/yellow_key_sprite_sheet.png").convert_alpha()
+
+cyan_key_sprite_sheet = pg.image.load(
+    "../../../assets/graphics/8-Bit Pixel Game/lvl4/cyan_key_sprite_sheet.png").convert_alpha()
+
+magenta_key_sprite_sheet = pg.image.load(
+    "../../../assets/graphics/8-Bit Pixel Game/lvl4/magenta_key_sprite_sheet.png").convert_alpha()
+
+key_mask = pg.mask.from_surface(yellow_key)  # Using the same mask to detect all keys cuz they have identical hit-boxes
 
 # Setting up Game Variables ------------------------------------------------------------------------------------
 
-maze_blit_position = [-797, 350]
+maze_blit_position = [-640, 575]
 
-keys_obtained = [0, 0, 0]  # These represent whether the player got the cyan, yellow and magenta keys respectively
+keys_obtained = [0, 0, 0]  # These represent whether the player got the cyan, magenta and yellow keys respectively
 
 last_anim_update = 0
 anim_frame = 0
@@ -53,25 +60,25 @@ def handle_key_input():
     if keys[pg.K_d] or keys[pg.K_RIGHT]:
         maze_blit_position[0] -= 5
 
-        if maze_mask.overlap(character_mask, (300 - maze_blit_position[0], 300 - maze_blit_position[1])):
+        if maze_mask.overlap(character_mask, (960 - maze_blit_position[0], 540 - maze_blit_position[1])) or maze_blit_position[0] <= -2235:  # Checks if player collides with maze or goes out of bounds
             maze_blit_position[0] += 5
 
     if keys[pg.K_a] or keys[pg.K_LEFT]:
         maze_blit_position[0] += 5
 
-        if maze_mask.overlap(character_mask, (300 - maze_blit_position[0], 300 - maze_blit_position[1])):
+        if maze_mask.overlap(character_mask, (960 - maze_blit_position[0], 540 - maze_blit_position[1])) or maze_blit_position[0] >= 970:  # Checks if player collides with maze or goes out of bounds
             maze_blit_position[0] -= 5
 
     if keys[pg.K_s] or keys[pg.K_DOWN]:
         maze_blit_position[1] -= 5
 
-        if maze_mask.overlap(character_mask, (300 - maze_blit_position[0], 300 - maze_blit_position[1])):
+        if maze_mask.overlap(character_mask, (960 - maze_blit_position[0], 540 - maze_blit_position[1])):
             maze_blit_position[1] += 5
 
     if keys[pg.K_w] or keys[pg.K_UP]:
         maze_blit_position[1] += 5
 
-        if maze_mask.overlap(character_mask, (300 - maze_blit_position[0], 300 - maze_blit_position[1])):
+        if maze_mask.overlap(character_mask, (960 - maze_blit_position[0], 540 - maze_blit_position[1])):
             maze_blit_position[1] -= 5
 
     if keys[pg.K_RETURN]:
@@ -79,10 +86,21 @@ def handle_key_input():
         maze_blit_position[0], maze_blit_position[1] = map(int, input("Enter Teleport co-ords: ").split())
 
 
-def check_key_gotten():
-    if yellow_key_mask.overlap(character_mask,
-                               (300 - maze_blit_position[0] - 1087, 300 - maze_blit_position[1] - 1072)):
+def check_if_keys_obtained():
+    if key_mask.overlap(character_mask, (960 - maze_blit_position[0] - 2727, 540 - maze_blit_position[1] - 583)):  # Key masks are the same but the offset decides whether we collided with key or not
+        keys_obtained[0] = 1
+
+    if key_mask.overlap(character_mask, (960 - maze_blit_position[0] - 775, 540 - maze_blit_position[1] - 1796)):
         keys_obtained[1] = 1
+
+    if key_mask.overlap(character_mask,(960 - maze_blit_position[0] - 1590, 540 - maze_blit_position[1] - 1564)):
+        keys_obtained[2] = 1
+
+    if -2620 < maze_blit_position[1] <= -2585:
+        if all(keys_obtained):
+            print("Level Cleared!")
+        else:
+            print("You need all three keys to unlock this door")
 
 
 def draw_game_elements():
@@ -91,10 +109,22 @@ def draw_game_elements():
     screen.blit(maze, maze_blit_position)
     screen.blit(maze_3d_effect, (maze_blit_position[0] + 3, maze_blit_position[1]))
 
-    screen.blit(character_surf, (300, 300))
-    if not keys_obtained[1]: screen.blit(yellow_key, (maze_blit_position[0] + 1087, maze_blit_position[1] + 1072))
+    screen.blit(character_surf, (960, 540))
 
-    screen.blit(sprite_sheet_yellow, (0, 0), (0, 41*anim_frame, 33, 41))
+    if not keys_obtained[0]:
+        screen.blit(cyan_key_sprite_sheet,
+                    (maze_blit_position[0] + 2725, maze_blit_position[1] + 581),
+                    (0, 41 * anim_frame, 33, 41))
+
+    if not keys_obtained[1]:
+        screen.blit(magenta_key_sprite_sheet,
+                    (maze_blit_position[0] + 775, maze_blit_position[1] + 1796),
+                    (0, 41 * anim_frame, 33, 41))
+
+    if not keys_obtained[2]:
+        screen.blit(yellow_key_sprite_sheet,
+                    (maze_blit_position[0] + 1590, maze_blit_position[1] + 1559),
+                    (0, 41 * anim_frame, 33, 41))
 
 
 def animate_keys():
@@ -121,10 +151,15 @@ while True:
             exit()
 
     handle_key_input()
-    check_key_gotten()
-    animate_keys()
+    check_if_keys_obtained()
 
+    animate_keys()
     draw_game_elements()
 
     pg.display.update()
     clock.tick(60)
+
+# TP to Cyan Key: -1855 -100
+# TP to Magenta Key: 250 -1250
+# TP to Yellow Key: -640 -1000
+# TP to Exit Door: -645 -2580
